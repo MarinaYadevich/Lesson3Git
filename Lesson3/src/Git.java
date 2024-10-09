c850e43c550c45e42943c6bd11d3eb0947d07213
Marina Yadevich <marishka.kazakova.2012@mail.ru>
2024-10-09 13:05:19 +0300

    9 октября commit in branch Git2

--- Lesson3/src/Git.java
+++ Lesson3/src/Git.java
@@ -1,9 +1,10 @@
 public class Git {
-}
 
-public static void main(String[] args) {
-    System.out.println("commit");
-    System.out.println("Привет");
-    System.out.println("Пока");
-    System.out.println("Пока");
+
+    public static void main(String[] args) {
+        System.out.println("commit");
+        System.out.println("Привет");
+        System.out.println("Пока");
+        System.out.println("Пока");
+    }
 }
